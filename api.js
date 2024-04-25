f8685ce11ba6f32891410550e691f229a52bb087
Marcelo Petroni <105806830+marcelopetroni@users.noreply.github.com>
2024-04-25 12:46:28 +0000

    cep api

---
+++ api.js
@@ -0,0 +1,20 @@
+const api = async () => {
+    
+    const url = "https://viacep.com.br/ws/";
+
+    try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error('Erro ao obter os dados.');
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Ocorreu um erro:', error);
+        throw error;
+    }
+};
+
+export default api;
