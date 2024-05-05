0e83d7b36942f1425632711f2e8c2ed8ac638935
Matheus Garcia <101107346+matheusmgs22@users.noreply.github.com>
2024-05-05 18:58:58 +0000

    colocando verificador no forms

--- forms.js
+++ forms.js
@@ -2,17 +2,23 @@
 Parse.initialize("v6UGSvLazH3FwPjOZPHajXTAFWyxelCPi1BJ5HAn", "SQzEsdUghX0atms3dAziOGhDPyd3lDDu9wGPeAYn");
 Parse.serverURL = "https://parseapi.back4app.com/";
 
-
 document.addEventListener('DOMContentLoaded', function () {
     var forms = document.querySelector('.forms-newsletter');
     var campo = document.querySelector('.form-message');
     var campo2 = document.querySelector('.form-email');
     var campo3 = document.querySelector('.form-textarea');
+    var errorMessage = document.querySelector('.error-message');
 
     forms.addEventListener('submit', async function(e) {
         e.preventDefault();
 
-        // Criar um objeto Parse para armazenar os dados do formulário
+
+        if (!campo.value || !campo2.value || !campo3.value) {
+            errorMessage.textContent = "Por favor, preencha todos os campos obrigatórios!";
+            errorMessage.style.display = "block";
+            return;
+        }
+
         var Formulario = Parse.Object.extend("Formulario");
         var formulario = new Formulario();
         formulario.set("mensagem", campo.value);
@@ -20,14 +26,16 @@ document.addEventListener('DOMContentLoaded', function () {
         formulario.set("texto", campo3.value);
 
         try {
-            // Salvar os dados no banco de dados do Parse
+
             await formulario.save();
             console.log("Dados do formulário salvos com sucesso!");
 
-            // Limpar os campos do formulário após salvar
             campo.value = '';
             campo2.value = '';
             campo3.value = '';
+
+            errorMessage.textContent = "";
+            errorMessage.style.display = "none";
         } catch (error) {
             console.error("Erro ao salvar dados do formulário:", error);
             throw error;
